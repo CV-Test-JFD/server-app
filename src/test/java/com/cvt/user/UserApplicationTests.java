ccf9ff24587c86b801a498eaf9fe5bdcfecf03df
dhayananth <dhayanthdharma@gmail.com>
2021-06-21 14:49:40 +0200

    Initialise the project

---
+++ src/test/java/com/cvt/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvt.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
